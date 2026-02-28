f269e6f7e092733455bb08c4f1bc24c5af3e7638
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-02-28 09:08:56 +0100

    Add actuator

--- lemur-app/build.gradle.kts
+++ lemur-app/build.gradle.kts
@@ -13,16 +13,20 @@ repositories {
 dependencies {
     implementation(platform(project(":lemur-libs:lemur-bom")))
 
+    implementation(libs.spring.boot.starter.actuator)
     implementation(libs.spring.boot.starter.data.jpa)
     implementation(libs.spring.boot.starter.kafka)
     implementation(libs.spring.boot.starter.webmvc)
     implementation(libs.spring.boot.starter.validation)
     implementation(libs.problem4j.spring.webmvc)
 
+    implementation(libs.micrometer.registry.prometheus)
+
     runtimeOnly(libs.postgresql)
 
     testImplementation(project(":lemur-libs:lemur-migration"))
     testImplementation(project(":lemur-libs:lemur-testkit"))
+    testImplementation(libs.spring.boot.starter.actuator.test)
     testRuntimeOnly(libs.junit.platform.launcher)
     testRuntimeOnly(libs.postgresql)
 }
