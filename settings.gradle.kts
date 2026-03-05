29ba09089e1453224f58d452ebdc9f3b253b3a86
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-03-05 20:56:30 +0100

    Add constraints on module names

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,3 +18,47 @@ include(":lemur-libs:lemur-log4j2")
 include(":lemur-libs:lemur-migration")
 include(":lemur-libs:lemur-model")
 include(":lemur-libs:lemur-testkit")
+
+/**
+ * Duplicate project names have some side effects. This function verifies that there are no duplicate project names in
+ * the build. Throws an exception if duplicates are found.
+ */
+fun verifyProjectNameDuplicates(project: ProjectDescriptor) {
+    val projectsByName = mutableMapOf<String, MutableList<ProjectDescriptor>>()
+
+    collectProjectNames(project, projectsByName)
+
+    val duplicates = projectsByName.filterValues { it.size > 1 }
+
+    check(duplicates.isEmpty()) {
+        buildString {
+            appendLine("Duplicate project names are not allowed:")
+            duplicates.forEach { (name, projects) ->
+                appendLine("  '$name' used by: ${projects.joinToString { it.path }}")
+            }
+        }
+    }
+}
+
+/**
+ * Collects project names into the provided map.
+ */
+fun collectProjectNames(project: ProjectDescriptor, names: MutableMap<String, MutableList<ProjectDescriptor>>) {
+    names.computeIfAbsent(project.name) { mutableListOf() }.add(project)
+    project.children.forEach { collectProjectNames(it, names) }
+}
+
+/**
+ * Changes build file names of all child projects to `<project-name>.gradle.kts`.
+ */
+fun changeChildrenBuildFileNames(project: ProjectDescriptor) {
+    project.children.forEach { changeChildrenBuildFileName(it) }
+}
+
+/**
+ * Changes build file name of the given project and all its children to `<project-name>.gradle.kts`.
+ */
+fun changeChildrenBuildFileName(project: ProjectDescriptor) {
+    project.buildFileName = "${project.name}.gradle.kts"
+    project.children.forEach { child -> changeChildrenBuildFileName(child) }
+}
