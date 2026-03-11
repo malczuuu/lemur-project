af51777555f5912a51a4567d63530d5869258df3
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-03-11 18:48:52 +0100

    Init Gradle project

---
+++ settings.gradle.kts
@@ -0,0 +1,41 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
+}
+
+rootProject.name = "lemur-project"
+
+/**
+ * Duplicate project names have some side effects. This function verifies that there are no duplicate project names in
+ * the build. Throws an exception if duplicates are found.
+ */
+fun verifyProjectNameDuplicates(project: ProjectDescriptor) {
+    val projectsByName = mutableMapOf<String, MutableList<ProjectDescriptor>>()
+
+    collectProjectNames(project, projectsByName)
+
+    val duplicates = projectsByName.filterValues { it.size > 1 }
+
+    check(duplicates.isEmpty()) {
+        buildString {
+            appendLine("Duplicate project names are not allowed:")
+            duplicates.forEach { (name, projects) ->
+                appendLine("  '$name' used by: ${projects.joinToString { it.path }}")
+            }
+        }
+    }
+}
+
+/**
+ * Collects project names into the provided map.
+ */
+fun collectProjectNames(project: ProjectDescriptor, names: MutableMap<String, MutableList<ProjectDescriptor>>) {
+    names.computeIfAbsent(project.name) { mutableListOf() }.add(project)
+    project.children.forEach { collectProjectNames(it, names) }
+}
